fda129d2b9b8cac9c57dee2d13fa5b1ec5544f81
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-19 14:11:26 +0000

    Seg fault, trying to read tmp_cells in between rebound and collision

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -247,6 +247,12 @@ int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obst
   checkError(err, "reading tmp_cells data", __LINE__);
 
   rebound(params, cells, tmp_cells, obstacles, ocl);
+  
+  err = clEnqueueReadBuffer(
+    ocl.queue, ocl.tmp_cells, CL_TRUE, 0,
+    sizeof(t_speed) * params.nx * params.ny, tmp_cells, 0, NULL, NULL);
+  checkError(err, "reading tmp_cells data", __LINE__);
+  
   collision(params, cells, tmp_cells, obstacles, ocl);
   return EXIT_SUCCESS;
 }
