b68047fd0997de609ae4c7726e68555fc995f880
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-20 21:19:28 +0000

    Still reading wrong i think

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -240,21 +240,21 @@ int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obst
 
   accelerate_flow(params, cells, obstacles, ocl);
   propagate(params, cells, tmp_cells, ocl);
-  rebound(params, cells, tmp_cells, obstacles, ocl);
 
     // Read tmp_cells from device
   err = clEnqueueReadBuffer(
     ocl.queue, ocl.tmp_cells, CL_TRUE, 0,
     sizeof(t_speed) * params.nx * params.ny, tmp_cells, 0, NULL, NULL);
   checkError(err, "reading tmp_cells data", __LINE__);
-   
-  collision(params, cells, tmp_cells, obstacles, ocl);
+  rebound(params, cells, tmp_cells, obstacles, ocl);
 
       // Read cells from device
   err = clEnqueueReadBuffer(
     ocl.queue, ocl.cells, CL_TRUE, 0,
     sizeof(t_speed) * params.nx * params.ny, cells, 0, NULL, NULL);
   checkError(err, "reading cells data", __LINE__);
+   
+  collision(params, cells, tmp_cells, obstacles, ocl);
   return EXIT_SUCCESS;
 }
 
