362335043b0bea5bb4946dfde92d6606032853b1
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-20 20:32:37 +0000

    Why do i do this degree

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -257,11 +257,11 @@ int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obst
 
   collision(params, cells, tmp_cells, obstacles, ocl);
 
-//     // Read cells from device
-//   err = clEnqueueReadBuffer(
-//     ocl.queue, ocl.cells, CL_TRUE, 0,
-//     sizeof(t_speed) * params.nx * params.ny, cells, 0, NULL, NULL);
-//   checkError(err, "reading cells data", __LINE__);
+    // Read cells from device
+  err = clEnqueueReadBuffer(
+    ocl.queue, ocl.cells, CL_TRUE, 0,
+    sizeof(t_speed) * params.nx * params.ny, cells, 0, NULL, NULL);
+  checkError(err, "reading cells data", __LINE__);
    
   return EXIT_SUCCESS;
 }
