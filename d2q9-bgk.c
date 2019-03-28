a571e9e7adbaaa22381bd430cadfc29a0e7a2a21
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-28 17:07:54 +0000

    I think i've found whats going wrong, but not sure as to why

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -429,6 +429,7 @@ float av_velocity(const t_param params, t_speed* cells, int* obstacles, t_ocl oc
       printf("sum_cells[%d]: %f\n", x, sum_u[x]);
       tot_u += sum_u[x];
   }
+  printf("sum_cells[%d]: %f\n", 17, sum_u[17]);
   // printf("Num_wkg: %d\n", (int)params.num_wkg);
   // printf("tot_cells: %d\n", (int)tot_cells);
   free(sum_cells);
