88415e105dae0d28e4e0238f93ac85eb1931fe43
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-28 16:11:26 +0000

    I need to sort my life out

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -139,7 +139,7 @@ int finalise(const t_param* params, t_speed** cells_ptr, t_speed** tmp_cells_ptr
 float total_density(const t_param params, t_speed* cells);
 
 /* compute average velocity */
-float av_velocity(const t_param params, t_speed* cells, int* obstacles);
+float av_velocity(const t_param params, t_speed* cells, int* obstacles, t_ocl ocl);
 
 /* calculate Reynolds number */
 float calc_reynolds(const t_param params, t_speed* cells, int* obstacles, t_ocl ocl);
@@ -443,8 +443,7 @@ int collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obs
 //   return tot_u / (float)tot_cells;
 
 // }
-
-float av_velocity(const t_param params, t_speed* cells, int* obstacles)
+float av_velocity(const t_param params, t_speed* cells, int* obstacles, t_ocl ocl)
 {
   int    tot_cells = 0;  /* no. of cells used in calculation */
   float tot_u;          /* accumulated magnitudes of velocity for each cell */
@@ -491,6 +490,7 @@ float av_velocity(const t_param params, t_speed* cells, int* obstacles)
       }
     }
   }
+
   printf("%d\n", tot_cells);
   return tot_u / (float)tot_cells;
 }
