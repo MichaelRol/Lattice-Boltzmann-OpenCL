420a0ff8c6666c404526f6c23deec8f18cd4f6f3
MichaelRol <michael.rollins@hotmail.co.uk>
2019-04-03 20:18:53 +0100

    Added more errors

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -782,7 +782,7 @@ float calc_reynolds(const t_param params, float* cells, int* obstacles)
 {
   const float viscosity = 1.f / 6.f * (2.f / params.omega - 1.f);
 
-  return av_velocity(params, cell, obstacles) * params.reynolds_dim / viscosity;
+  return av_velocity(params, cells, obstacles) * params.reynolds_dim / viscosity;
 }
 
 float total_density(const t_param params, float* cells)
