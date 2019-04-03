a28b7da599a9f210e4dcf4874d8c3932562a6f2a
MichaelRol <michael.rollins@hotmail.co.uk>
2019-04-03 17:13:50 +0100

    Weirdly didn't change anything

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -221,8 +221,8 @@ int main(int argc, char* argv[])
 
   for (int tt = 0; tt < params.maxIters; tt += 2)
   {
-    av_vels[tt] = timestep_first(params, ocl, 15872);
-    av_vels[tt+1] = timestep_second(params, ocl ,15872);
+    av_vels[tt] = timestep_first(params, ocl, 1);
+    av_vels[tt+1] = timestep_second(params, ocl ,1);
 #ifdef DEBUG
     printf("==timestep: %d==\n", tt);
     printf("av velocity: %.12E\n", av_vels[tt]);
